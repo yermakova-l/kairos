6f076873720161fbe1ac32a8fdf0b19f33c3370a
yermakova-l <lyudmyla.yermakova@gmail.com>
2020-12-10 17:28:56 +0200

    Block Year Report added, insurance block deleted

--- js/main.js
+++ js/main.js
@@ -58,26 +58,7 @@
         lastScrollTop = st;
     }
 })();
-/*
-(function (){
-    $('.toggle').click(function(e) {
-  	e.preventDefault();
-  
-    var $this = $(this);
-  
-    if ($this.next().hasClass('show')) {
-        $this.next().removeClass('show');
-        $this.next().slideUp(350);
-    } else {
-        $this.parent().parent().find('.inner').removeClass('show');
-        $this.parent().parent().find('.inner').slideUp(350);
-        $this.next().toggleClass('show');
-        $this.next().slideToggle(350);
-    }
-});
-    
-})();
-*/
+
 
  $(function() {
         $("#accordion").accordion({
